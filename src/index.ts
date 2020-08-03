deab2b647ee4c509e15f8e9aa35b93bc060b9a60
Tsareff <tsareff.sergey@gmail.com>
2020-08-03 17:37:30 +0300

    added expire logic

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
-import { createEvent, Store } from "effector";
+import { createEvent, Store } from 'effector';
 
 type PersistConfig = {
   key?: string;
-}
+  expire?: number;
+};
 
 const defaultConfig = {
   key: 'persist'
 };
 
-export const withPersist = <State>(store: Store<State>, config: PersistConfig = defaultConfig) => {
+export const withPersist = <State>(
+  store: Store<State>,
+  config: PersistConfig = defaultConfig
+) => {
   const name = store.shortName;
-  const persistKey = `${config.key}:${name}`;
-  const snapshot = localStorage.getItem(persistKey);
+  const { key, expire } = config;
+  const persistKey = `${key}:${name}`;
   const rehydrate = createEvent('@PERSIST/REHYDRATE');
+
+  if (expire && isExpired(expire)) {
+    localStorage.removeItem(persistKey);
+  }
+
+  const snapshot = localStorage.getItem(persistKey);
 
   if (snapshot) {
     store.on(rehydrate, () => JSON.parse(snapshot));
     rehydrate();
   }
 
-
-  store.watch(state => {
+  store.watch((state) => {
     localStorage.setItem(persistKey, JSON.stringify(state));
   });
 
   return store;
 };
+
+const isExpired = (expire: number) => expire < Date.now();
